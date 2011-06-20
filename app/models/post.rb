91e349d304b22e16e9ec5af266300eecadd0fbea
bullfight <p.schmitz@gmail.com>
2011-06-20 08:21:58 +0200

    prior to push

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,14 @@
 class Post < ActiveRecord::Base
-  attr_accessible :name, :description, :rsvp_by, :date_of,  :contact, :category_id, :venue_id
+  attr_accessible :name, :description, :rsvp_by, :date_of,  :contact, 
+    :category_id, :category_attributes, :venue_id, :venue_attributes
   
   belongs_to :user
   belongs_to :category
   belongs_to :venue
   
   validates_presence_of :name, :date_of, :contact, :venue_id
+  
+  accepts_nested_attributes_for :category, :venue
   
   def self.search(query)
     where("name like ?", "%#{query}%")
